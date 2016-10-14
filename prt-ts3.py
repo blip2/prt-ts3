11ad6e143aeb8a83f8a72beea9caa52bb71890b5
Ben Hussey <ben@blip2.net>
2016-10-14 23:07:05 +0100

    PHP Manual tool completed

--- prt-ts3.py
+++ prt-ts3.py
@@ -1,16 +1,17 @@
 # PRT TS3 Permission Setting API
 # blip2 - 2016
 
-ALLOWED_ADDR = ['127.0.0.1', '74.55.51.162', '94.175.121.72',]
-
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort
 from api import TS3Server
-from flask_cors import CORS 
+from flask_cors import CORS
 from config import *
 
 app = Flask(__name__)
 cors = CORS(app)
 server = TS3Server()
+
+ALLOWED_ADDR = ['127.0.0.1', '74.55.51.162', '94.175.121.72', ]
+
 
 @app.before_request
 def limit_remote_addr():
@@ -28,11 +29,12 @@ def error(json, status):
     response.status_code = status
     return response
 
+
 @app.route('/player/', methods=['OPTIONS'])
 def player_get():
     response = {"response": "success", }
     return jsonify(**response)
-    
+
 
 @app.route('/player/', methods=['PUT'])
 def player_update():
@@ -40,26 +42,27 @@ def player_update():
     print request.data
     data = request.get_json()
     if not data:
-        return error({"response": "no data received",}, 400)
+        return error({"response": "no data received", }, 400)
     if "uid" not in data or "team" not in data or "rank" not in data:
-        return error({"response": "missing parameters from request",}, 400)
+        return error({"response": "missing parameters from request", }, 400)
     if data["team"] not in TEAMS:
-        return error({"response": "invalid team reference",}, 400)
+        return error({"response": "invalid team reference", }, 400)
     team = TEAMS[data["team"]]
     if data["rank"] not in RANKS:
-        return error({"response": "invalid rank reference",}, 400)
+        return error({"response": "invalid rank reference", }, 400)
     rank = RANKS[data["rank"]]
 
     clientid = server.getClientId(data["uid"])
     if not clientid:
-        return error({"response": "UID not found on Teamspeak server",}, 404)
-        
+        return error({"response": "UID not found on Teamspeak server", }, 404)
+
     server.addToServerGroup(clientid, team["groupid"])
     server.addToServerGroup(clientid, rank["groupIdbyTeam"][team["id"]])
     if "additionalgroups" in rank:
         for groupid in rank["additionalgroups"][team["id"]]:
             server.addToServerGroup(clientid, groupid)
-    server.changeChannelGroup(clientid, team["channelid"], CHANNEL_GROUPS[rank["channelGroup"]]["groupid"])
+    server.changeChannelGroup(clientid, team["channelid"],
+                              CHANNEL_GROUPS[rank["channelGroup"]]["groupid"])
 
     response = {"response": "success", }
     return jsonify(**response)
@@ -69,24 +72,26 @@ def player_update():
 def player_delete():
     data = request.get_json()
     if not data:
-        return error({"response": "no data received",}, 400)
+        return error({"response": "no data received", }, 400)
     if "uid" not in data:
-        return error({"response": "missing parameters from request",}, 400)
+        return error({"response": "missing parameters from request", }, 400)
 
     clientid = server.getClientId(data["uid"])
     if not clientid:
-        return error({"response": "UID not found on Teamspeak server",}, 404)
-    
+        return error({"response": "UID not found on Teamspeak server", }, 404)
+
     for team in TEAMS:
         team = TEAMS[team]
         for rank in RANKS:
             rank = RANKS[rank]
             server.removeFromServerGroup(clientid, team["groupid"])
-            server.removeFromServerGroup(clientid, rank["groupIdbyTeam"][team["id"]])
+            server.removeFromServerGroup(clientid,
+                                         rank["groupIdbyTeam"][team["id"]])
             if "additionalgroups" in rank:
                 for groupid in rank["additionalgroups"][team["id"]]:
                     server.removeFromServerGroup(clientid, groupid)
-            server.changeChannelGroup(clientid, team["channelid"], CHANNEL_GROUPS[0]["groupid"])
+            server.changeChannelGroup(clientid, team["channelid"],
+                                      CHANNEL_GROUPS[0]["groupid"])
 
     response = {"response": "success", }
     return jsonify(**response)
@@ -96,9 +101,9 @@ def player_delete():
 def get_battlemode():
     data = request.get_json()
     if "team" not in data:
-        return error({"response": "missing parameters from request",}, 400)
+        return error({"response": "missing parameters from request", }, 400)
     if data["team"] not in TEAMS:
-        return error({"response": "invalid team reference",}, 400)
+        return error({"response": "invalid team reference", }, 400)
     team = TEAMS[data["team"]]
 
     subPower = server.getSubscribePower(team["channelid"])
@@ -107,7 +112,7 @@ def get_battlemode():
     elif int(subPower) == 0:
         response = {"response": "success", "state": "off", }
     else:
-        response = {"response": "success", "state": "unknown",}
+        response = {"response": "success", "state": "unknown", }
     return jsonify(**response)
 
 
@@ -115,20 +120,22 @@ def get_battlemode():
 def set_battlemode():
     data = request.get_json()
     if "team" not in data or "state" not in data:
-        return error({"response": "missing parameters from request",}, 400)
+        return error({"response": "missing parameters from request", }, 400)
     if data["team"] not in TEAMS:
-        return error({"response": "invalid team reference",}, 400)
+        return error({"response": "invalid team reference", }, 400)
     team = TEAMS[data["team"]]
     if data["state"] not in ["on", "off"]:
-        return error({"response": "invalid state",}, 400)
+        return error({"response": "invalid state", }, 400)
 
     if data["state"] == "on":
         server.setSubscribePower(team["channelid"], BATTLEMODE_POWER)
-        server.sendServerMessage(team["name"] + " channels are now in BATTLEMODE")
+        server.sendServerMessage(team["name"] +
+                                 " channels are now in BATTLEMODE")
 
     if data["state"] == "off":
         server.setSubscribePower(team["channelid"], 0)
-        server.sendServerMessage(team["name"] + " channels are no longer in BATTLEMODE")
+        server.sendServerMessage(team["name"] +
+                                 " channels are no longer in BATTLEMODE")
 
     response = {"response": "success", }
     return jsonify(**response)
--- www/index.php
+++ www/index.php
@@ -1,9 +1,179 @@
 <?php
 
-?>
-<html>
-<body>
-<h1>PRT Teamspeak Authentication</h1>
+$ranks = array("reserve", "grunt", "nco", "sl", "hco", "xo", "co", "sco");
 
+$EMC_PASS = "g2UBFaRLqjK";
+$APN_PASS = "s2diooCnBEk";
+$ADM_PASS = "YI7IM0gTnJo";
+
+if ($_POST) {
+    if ($_POST["password"] == $EMC_PASS) {
+        if ($_POST["team"] != "emc") {
+            $response = array('danger', 'Invalid Team Selected');
+        }
+    } elseif ($_POST["password"] == $APN_PASS) {
+        if ($_POST["team"] != "apn") {
+            $response = array('danger', 'Invalid Team Selected');
+        }
+    } elseif ($_POST["password"] == $ADM_PASS) {
+        $response = "";
+    } else {
+        $response = array('danger', 'Authentication Failed');
+    }
+}
+
+
+if (!$response && $_POST) {
+    if ($_POST["team"] == "remove") {
+        $postData = array(
+            'uid' => $_POST["uid"],
+        );
+
+        $ch = curl_init('http://prt.blip2.net:7215/player/');
+        curl_setopt_array($ch, array(
+            CURLOPT_CUSTOMREQUEST => "DELETE",
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+            CURLOPT_POSTFIELDS => json_encode($postData)
+        ));
+
+        $json = curl_exec($ch);
+        if($json === FALSE){
+            $response = array('danger', 'Failed to talk to Teamspeak server');
+        } else {
+            $ret = json_decode($json, true);
+            if ($ret['response'] == "success") {
+                $response = array('success', "Permissions removed.");
+            } else {
+                $response = array('danger', $ret['response']);
+            }
+        }
+
+    } else {
+
+        $postData = array(
+            'uid' => $_POST["uid"],
+            'team' => $_POST["team"],
+            'rank' => $_POST["rank"],
+        );
+
+        $ch = curl_init('http://prt.blip2.net:7215/player/');
+        curl_setopt_array($ch, array(
+            CURLOPT_CUSTOMREQUEST => "PUT",
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+            CURLOPT_POSTFIELDS => json_encode($postData)
+        ));
+
+        $json = curl_exec($ch);
+        if($json === FALSE){
+            $response = array('danger', 'Failed to talk to Teamspeak server');
+        } else {
+            $ret = json_decode($json, true);
+            if ($ret['response'] == "success") {
+                $response = array('success', "Permissions assigned.");
+            } else {
+                $response = array('danger', $ret['response']);
+            }
+        }
+
+    }
+
+}
+
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PRT Teamspeak Permissions</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <style>
+    body {
+      padding-top: 40px;
+      padding-bottom: 40px;
+      background-color: #eee;
+    }
+
+    .form-signin {
+      max-width: 450px;
+      padding: 15px;
+      margin: 0 auto;
+    }
+    .form-signin .form-signin-heading,
+    .form-signin .checkbox {
+      margin-bottom: 10px;
+    }
+    .form-signin .checkbox {
+      font-weight: normal;
+    }
+    .form-signin .form-control {
+      position: relative;
+      height: auto;
+      -webkit-box-sizing: border-box;
+         -moz-box-sizing: border-box;
+              box-sizing: border-box;
+      padding: 10px;
+      font-size: 16px;
+    }
+    .form-signin .form-control:focus {
+      z-index: 2;
+    }
+    .form-signin input[type="email"] {
+      margin-bottom: -1px;
+      border-bottom-right-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+    .form-signin input[type="password"] {
+      margin-bottom: 10px;
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
+    </style>
+</head>
+<body>
+<div class="container">
+
+  <form class="form-signin" action="." method="post">
+    <h2 class="form-signin-heading">PRT Teamspeak Permissions</h2>
+    <?php
+    if ($response) {
+        echo '<div class="alert alert-'.$response[0].'">'.$response[1].'</div>';
+    }
+    ?>
+    <p>
+        <label for="password" class="sr-only">password</label>
+        <input type="password" id="password" name="password" class="form-control" placeholder="password" value="<?=$_POST["password"]?>" required autofocus>
+    </p>
+    <p>
+        <label for="uid" class="sr-only">uid</label>
+        <input type="text" id="uid" name="uid" class="form-control" placeholder="Teamspeak UID" required>
+    </p>
+    <div class="radio">
+      <label style="margin-right: 20px;"><input type="radio" name="team" value="emc"<?php if ($_POST["team"]=="emc") echo ' checked="checked"'; ?>/>EMC</label>
+      <label style="margin-right: 20px;"><input type="radio" name="team" value="apn"<?php if ($_POST["team"]=="apn") echo ' checked="checked"'; ?> />APN</label>
+      <label><input type="radio" name="team" value="remove"<?php if ($_POST["team"]=="remove") echo ' checked="checked"'; ?> />REMOVE</label>
+    </div>
+    <div class="form-group">
+      <label for="rank" class="sr-only">Select list:</label>
+      <select class="form-control" name="rank" id="rank">
+        <?php
+        foreach ($ranks as &$rank) {
+            $selected = "";
+            if ($_POST["rank"] == $rank) $selected = ' selected="selected"';
+            echo '<option value="'.$rank.'"'.$selected.'>'.$rank.'</option>';
+        }
+        ?>
+      </select>
+    </div>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Go</button>
+  </form>
+
+</div>
 </body>
 </html>
